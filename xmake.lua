57282da9ab59aa599d7db29975a233417724049d
Tab Nahida <tabye320@outlook.com>
2026-02-06 19:19:46 +0000

    new project structure

--- xmake.lua
+++ xmake.lua
@@ -8,7 +8,7 @@ add_requires("zlib")
 
 target("byte_bpe_train")
     set_kind("binary")
-    add_files("byte_bpe_train.cpp", "byte_bpe_lib.cpp", "byte_bpe_bpe.cpp")
+    add_files("BPE_trainer/**.cpp")
     set_rundir("$(projectdir)")
     add_packages("zlib")
     
